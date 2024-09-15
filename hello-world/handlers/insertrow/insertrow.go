fbbaf5cf453e59be7723fbb27cd1ec36cd47542c
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-09-15 14:20:25 +0330

    ~

--- hello-world/handlers/insertrow/insertrow.go
+++ hello-world/handlers/insertrow/insertrow.go
@@ -1,10 +1,10 @@
-package insertrow
+package main
 
 import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
+	// "strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,38 +19,38 @@ func initRedis() *redis.Client {
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	rdb := initRedis()
+	// rdb := initRedis()
 
-	// Get the current value of "cnt" from Redis
-	cntVal, err := rdb.Get(ctx, "cnt").Result()
-	if err == redis.Nil {
-		// Key does not exist, initialize with 0
-		cntVal = "0"
-	} else if err != nil {
-		log.Printf("Error fetching 'cnt' from Redis: %v", err)
-		return events.APIGatewayProxyResponse{
-			Body:       "Internal Server Error",
-			StatusCode: 500,
-		}, nil
-	}
+	// // Get the current value of "cnt" from Redis
+	// cntVal, err := rdb.Get(ctx, "cnt").Result()
+	// if err == redis.Nil {
+	// 	// Key does not exist, initialize with 0
+	// 	cntVal = "0"
+	// } else if err != nil {
+	// 	log.Printf("Error fetching 'cnt' from Redis: %v", err)
+	// 	return events.APIGatewayProxyResponse{
+	// 		Body:       "Internal Server Error",
+	// 		StatusCode: 500,
+	// 	}, nil
+	// }
 
-	// Convert the current count to an integer, increment it
-	cnt, _ := strconv.Atoi(cntVal)
-	cnt++
+	// // Convert the current count to an integer, increment it
+	// cnt, _ := strconv.Atoi(cntVal)
+	// cnt++
 
 
-	// Update the "cnt" key in Redis
-	err = rdb.Set(ctx, "cnt", cnt, 0).Err()
-	if err != nil {
-		log.Printf("Error updating 'cnt' in Redis: %v", err)
-		return events.APIGatewayProxyResponse{
-			Body:       "Internal Server Error",
-			StatusCode: 500,
-		}, nil
-	}
+	// // Update the "cnt" key in Redis
+	// err = rdb.Set(ctx, "cnt", cnt, 0).Err()
+	// if err != nil {
+	// 	log.Printf("Error updating 'cnt' in Redis: %v", err)
+	// 	return events.APIGatewayProxyResponse{
+	// 		Body:       "Internal Server Error",
+	// 		StatusCode: 500,
+	// 	}, nil
+	// }
 
-	greeting := fmt.Sprintf("Hello! You are visitor number %d.\n", cnt)
-
+	greeting := fmt.Sprintf("Hello! You are visitor number %d.\n", 1)
+	log.Printf("greeting => %s", greeting)
 	return events.APIGatewayProxyResponse{
 		Body:       greeting,
 		StatusCode: 200,
