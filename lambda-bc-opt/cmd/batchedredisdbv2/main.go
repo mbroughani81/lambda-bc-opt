20b86ccdbefbf5f09275de1f065a2daea7438d0a
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-10-02 23:38:19 +0330

    ~

--- lambda-bc-opt/cmd/batchedredisdbv2/main.go
+++ lambda-bc-opt/cmd/batchedredisdbv2/main.go
@@ -9,5 +9,5 @@ func main() {
 	batchedRedisDB := db.ConsBatchedRedisDBV2("10.10.0.1:8080")
 	key := "cnt"
 	value, _ := batchedRedisDB.Get(key)
-	log.Println("key: %s, value: %s", key, value)
+	log.Printf("key: %s, value: %s", key, value)
 }
