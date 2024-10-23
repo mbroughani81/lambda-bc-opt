d8a0f27b5d8ddc4c009e9c2f5fb15c157773f864
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-10-23 13:49:39 +0330

    start of localLambda

---
+++ lambda-bc-opt/benchmark/localLambda/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const workerCount int = 1000
+const invCount int = 50000000
+
+func main() {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	handler := slog.NewTextHandler(os.Stdout, opts)
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
+	slog.Info("Starting Benchmark")
+	db := redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%s", "localhost", "6379"),
+		DB:   0,
+		PoolSize: 1,
+	})
+	slog.Info("DB connected")
+
+	// each of workers listern for the taskChan
+	var tasksChan chan string = make(chan string, invCount)
+	var done chan struct{} = make(chan struct{})
+
+	// creating the task
+	for i := 0; i < invCount; i++ {
+		tasksChan <- "Get"
+	}
+
+	slog.Info("Tasks are ready in tasksChan")
+	time.Sleep(5 * time.Second)
+
+	// creating the workers (each worker is a goroutine)
+	slog.Info("Running tasks: Starting")
+	start := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(workerCount) // Done when goroutine finished the work
+
+	ctx := context.Background()
+
+	for i := 0; i < workerCount; i++ {
+		f := func(goroutineId int) {
+			select {
+			case <-done:
+				return
+			case task := <-tasksChan: // a task is assigned
+				slog.Debug(fmt.Sprintf("task <%s> - goroutineId %d : Starting", task, goroutineId))
+				result := db.Get(ctx, "cnt")
+				slog.Debug(fmt.Sprintf("task <%s> - goroutineId %d : Done - result : %s", task, goroutineId, result))
+				wg.Done()
+			}
+		}
+		go f(i)
+	}
+	wg.Wait()
+	slog.Info("Running tasks: Done")
+	duration := time.Since(start)
+	slog.Info(fmt.Sprintf("Duration => %v", duration))
+	// log.SetOutput(io.Discard)
+	// http.HandleFunc("/getterNaive", getterHandler)
+
+	// log.Println("Starting server on :8080")
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	// }
+}
